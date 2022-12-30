ce330c5a92ab924b074288c1f3a252764e5b6323
Sergio Díaz <sdiazf99@outlook.com>
2022-12-30 17:25:15 -0600

    feature: Skeleton structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,58 @@
-import { Component } from '@angular/core';
+import { Component,OnInit, AfterViewInit, ViewChild } from '@angular/core';
+import { Product } from './Interfaces/product';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatDialog } from '@angular/material/dialog';
+import { ProductService } from './Services/product.service';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'FrontEndApp';
+export class AppComponent implements OnInit, AfterViewInit {
+  title = 'Supermarket';
+  displayedColumns:string[]= ['Index','Cód. Art', 'Nombre Art.', 'Des. Art.', 'Cant. Art.', ' ']
+  dataProduct = new MatTableDataSource<Product>();
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  constructor(
+    private _snackbar: MatSnackBar,
+    private _productService: ProductService
+  ){
+
+  }
+
+  applyFilter(event:Event){
+
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataProduct.filter = filterValue.trim().toLowerCase();
+
+  }
+
+  showProducts(){
+    this._productService.getList().subscribe({
+      next:(data)=>{
+        if(data.status){
+          this.dataProduct.data = data.value;
+        }
+      },
+      error:(e)=>{}
+    });
+  }
+
+  ngOnInit(): void {
+      
+    this.showProducts();
+  }
+
+  ngAfterViewInit(): void {
+      this.dataProduct.paginator = this.paginator;
+  }
+  
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,49 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+//*1. To work with Reactive Forms
+import { ReactiveFormsModule } from '@angular/forms';
+//*2. To work with http requests
+import { HttpClientModule } from '@angular/common/http';
+//*3. To work with material table
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+//*4. To work with form controls
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule} from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+//*5.- To work with alerts
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+//*6.- To work with icons
+import {MatIconModule} from '@angular/material/icon'
+//*7. To work with modals
+import {MatDialogModule} from '@angular/material/dialog'
+//*8. To work with Grids
+import {MatGridListModule} from '@angular/material/grid-list'
+
+
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatButtonModule,
+    MatSnackBarModule,
+    MatIconModule,
+    MatDialogModule,
+    MatIconModule,
+    MatGridListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
