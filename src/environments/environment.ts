6aa84dc71d57a4b30c0ae38111ed18fec3903101
Sergio Díaz <sdiazf99@outlook.com>
2022-12-30 17:11:57 -0600

    feature: Endpoint configuration to consume

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  //Put your uri localhost up backend project
+  endpoint: "http://localhost:5161/",
 };
 
 /*
