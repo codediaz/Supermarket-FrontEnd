e7a5c306b2a90d98355b13e3d0858b4dbec89fe0
Sergio Díaz <sdiazf99@outlook.com>
2022-12-30 17:16:42 -0600

    feature: Services and requests structure add

---
+++ src/app/Services/product.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+
+//Extends and import to use
+import { Product } from '../Interfaces/product';
+import { HttpClient } from "@angular/common/http";
+import { environment } from 'src/environments/environment';
+import { Observable } from 'rxjs';
+import { ResponseApi } from '../Interfaces/response-api';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  //Endpoit to connect
+  private endpoint:string = environment.endpoint;
+  private myApiUrl:string = this.endpoint + "api/product";
+
+  constructor(private http:HttpClient) { }
+
+  //*Service Request
+  getList():Observable<ResponseApi>{
+    
+    return this.http.get<ResponseApi>(this.myApiUrl)
+
+  }
+
+  add(request:Product):Observable<ResponseApi>{
+    
+    return this.http.post<ResponseApi>(this.myApiUrl, request)
+  }
+
+  update(request:Product):Observable<ResponseApi>{
+    
+    return this.http.put<ResponseApi>(this.myApiUrl, request)
+  }
+
+  delete(code:string):Observable<ResponseApi>{
+
+    return this.http.delete<ResponseApi>(`${this.myApiUrl}/${code}`)
+  }
+  
+}
